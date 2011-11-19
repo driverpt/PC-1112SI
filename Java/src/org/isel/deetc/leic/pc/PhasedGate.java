cec538d45db0663747478b7e5fc6ad3901ea70e2
unknown <Luis@LAB-01.(none)>
2011-11-19 19:08:05 +0000

    Initial Commit, added PhasedGate and TransientSignal

---
+++ Java/src/org/isel/deetc/leic/pc/PhasedGate.java
@@ -0,0 +1,50 @@
+package org.isel.deetc.leic.pc;
+
+public class PhasedGate {
+    private final Object _monitor = new Object();
+    private final int _maxParticipants;
+
+    private int _currentParticipants;
+    private boolean _isSignaled;
+    private boolean _isUsed;
+
+    public PhasedGate( int participants ) {
+        _maxParticipants = participants;
+        _currentParticipants = 0;
+        _isSignaled = false;
+    }
+
+    public void await() throws InterruptedException {
+        synchronized ( _monitor ) {
+            if ( _isUsed ) {
+                throw new IllegalStateException();
+            }
+            if ( ++_currentParticipants == _maxParticipants ) {
+                _isSignaled = true;
+                _isUsed     = true;
+                _monitor.notifyAll();
+            }
+            while ( true ) {
+                if ( _isSignaled ) {
+                    return;
+                }
+                try {
+                    _monitor.wait();
+                } catch ( InterruptedException e ) {
+                    removeParticipant();
+                    throw e;
+                }
+
+            }
+        }
+    }
+
+    public void removeParticipant() {
+        synchronized ( _monitor ) {
+            if ( _isUsed ) {
+                throw new IllegalStateException();
+            }
+            --_currentParticipants;
+        }
+    }
+}
---
+++ Java/src/org/isel/deetc/leic/pc/TransientSignal.java
@@ -0,0 +1,63 @@
+package org.isel.deetc.leic.pc;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TransientSignal {
+
+    private final Object _monitor = new Object();
+    private Queue< Waiter > _requests;
+
+    public TransientSignal() {
+        _requests = new LinkedList< Waiter >();
+    }
+
+    public boolean await() throws InterruptedException {
+        synchronized ( _monitor ) {
+            final Waiter request = new Waiter();
+            request.isSignaled = false;
+            _requests.add( request );
+            try {
+                while ( true ) {
+                    if ( request.isSignaled ) {
+                        _requests.remove( request );
+                        return true;
+                    }
+                    _monitor.wait();
+                }
+            } catch ( InterruptedException e ) {
+                Thread.currentThread().interrupt();
+                return false;
+            }
+        }
+
+    }
+
+    public void signalOne() {
+        synchronized ( _monitor ) {
+            if ( _requests.isEmpty() ) {
+                return;
+            }
+            final Waiter request = _requests.remove();
+            request.isSignaled = true;
+            _monitor.notifyAll();
+        }
+    }
+
+    public void signalAll() {
+        synchronized ( _monitor ) {
+            if ( _requests.isEmpty() ) {
+                return;
+            }
+            for ( final Waiter request : _requests ) {
+                request.isSignaled = true;
+                _requests.remove( request );
+            }
+            _monitor.notifyAll();
+        }
+    }
+
+    private static class Waiter {
+        public boolean isSignaled;
+    }
+}
