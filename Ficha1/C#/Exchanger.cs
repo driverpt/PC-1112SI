a79404f91ff3938a24197fa272f11d46723cd8d5
unknown <Luis@Luis-Portatil.(none)>
2011-11-20 22:08:27 +0000

    Added C# Visual Studio Project, .gitignore and Exchanger.cs untested

---
+++ Ficha1/C#/Exchanger.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+namespace CSharp
+{
+    public class Exchanger< T >
+    {
+        private readonly object _monitor = new object();
+        private Request _request;
+
+        public bool Exchange( T mine, int timeout, out T yours )
+        {
+            lock( _monitor )
+            {
+                if( _request != default( Request ) )
+                {
+                    yours        = _request.mine;
+                    _request.his = mine;
+                    _request     = default( Request );
+                    Monitor.PulseAll( _monitor );
+                }
+
+                Request request = new Request();
+                request.mine = mine;
+                _request = request;
+                
+                while( true )
+                {
+                    if ( !Monitor.Wait( _monitor, timeout ) )
+                    {
+                        yours = default( T );
+                        return false;
+                    }
+                    _request.mine = mine;
+                }
+            }
+        }
+
+        private class Request
+        {
+            public T mine;
+            public T his;
+        }
+    }
+}
