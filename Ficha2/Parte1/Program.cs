1b51012bc4a137094311cdb5f34fcd32e7dc54fc
Luis Miguel Dias Duarte <lduarte@luisduarte.net>
2012-02-15 17:41:49 +0000

    Added working Parallel.For custom implementation

--- Ficha2/Parte1/Program.cs
+++ Ficha2/Parte1/Program.cs
@@ -1,11 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Parte1
 {
     class Program
     {
+        public static ParallelLoopResult For( int start, int end, ParallelOptions options, Action<int> body )
+        {
+            // Following the HAPPY PATH :)
+            // Problem : If the division result is a decimal number
+            var segment = ( ( end - start ) / options.MaxDegreeOfParallelism );
+            var totalSplits = ( end - start ) / segment;
+
+            Task task = new Task( () =>
+            {
+                for ( int i = 0 ; i < totalSplits ; ++i )
+                {
+                    int i2 = i;
+                    Task.Factory.StartNew( () =>
+                                               {
+                                                   int i1 = i2;
+                                                   Console.WriteLine( "Start -> {0}", start + ( i1 * segment ) );
+                                                   Console.WriteLine( "End   -> {0}", start + segment + ( i1 * segment ) );
+                                                   for ( int j = start + ( i1 * segment ) ; j < start + segment + ( i1 * segment ) ; ++j )
+                                                   {
+                                                       body( j );
+                                                   }
+                                               },
+                                           TaskCreationOptions.AttachedToParent );
+                }
+            }
+            );
+            task.Start( options.TaskScheduler );
+            task.Wait( options.CancellationToken );
+
+            return Parallel.For(0, 0, (x) => Console.WriteLine( "Completed" ) );
+        }
+
         public static int NumberOfTasks { get; set; }
+
         public static void Alinea2()
         {
             Task t1 = Task.Factory.StartNew( () =>
@@ -47,9 +82,28 @@ namespace Parte1
             Console.WriteLine("{0} Tasks Ran", NumberOfTasks );
         }
 
+        public static void TestParallelLoop()
+        {
+            var options = new ParallelOptions
+                              {
+                                  CancellationToken = new CancellationToken(),
+                                  MaxDegreeOfParallelism = 4,
+                                  TaskScheduler = TaskScheduler.Default
+                              };
+            ParallelLoopResult result = Program.For(0, 100, options, (arr) => Console.Write(arr + "-") );
+            Console.WriteLine( result.IsCompleted );
+            
+        }
+
+        public static void RandomInt(int number)
+        {
+            number = 5;
+        }
+
         public static void Main( string[] args )
         {
-            Test();
+            //Test();
+            TestParallelLoop();
             Console.ReadKey();
         }
     }
